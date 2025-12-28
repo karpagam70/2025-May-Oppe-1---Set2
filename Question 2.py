e50e80448fe964acda9a7555149063677757beb4
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:22:15 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,11 @@
+'''
+Deinterleave Even and Odd Indices in String
+Write a function deinterleave that takes a string s as input and returns a new string where the characters at even indices are placed before the characters at odd indices.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples
+
+"abcdef" -> "acebdf"
+"12345" -> "13524"
+'''
