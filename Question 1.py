d76708f957f3cd6b37e51131a1ae1332b0d1f3e1
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:21:38 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,8 @@
+'''
+Check is even or divisible by 5
+Write a function is_even_or_divisible_by_5 that takes an integer as input and return True if it is even or is divisible by 5 else False.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example is_even_or_divisible_by_5(8) -> True (8 is even number) is_even_or_divisible_by_5(10) -> True (10 is even number and divisible by 5) is_even_or_divisible_by_5(15) -> True (15 is divisible by 5) is_even_or_divisible_by_5(7) -> False (not an even number nor divisible by 5)
+'''
