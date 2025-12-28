59e95a443acaae6fe0ab7fa288098b0fd16aea8f
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:25:36 +0530

    Create Question 7.py

---
+++ Question 7.py
@@ -0,0 +1,39 @@
+'''
+Pattern Printing - W Pattern
+Given an integer n (where n > 0), print an "W" shaped pattern with n rows.
+
+The pattern should consist of a vertical bar (|) on the left and right sides of each row. Additionally, it should have forward slashes (/) and backslashes () moving diagonally towards each other from the top to the bottom.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+A single integer n, representing the number of rows in the pattern.
+Output Format
+
+An "W" shaped pattern with n rows, as described.
+Examples
+
+Input:
+
+1
+Output:
+
+|/\|
+
+Input:
+
+2
+Output:
+
+| /\ |
+|/  \|
+Input:
+
+3
+Output:
+
+|  /\  |
+| /  \ |
+|/    \|
+'''
